1dcfde716396f0c725666cb9e18b4c8aab089af1
regokan <regokan@gmail.com>
2024-10-07 08:15:19 +0500

    refactor cache process attributes like pid, user, and command in Process class

--- include/process.h
+++ include/process.h
@@ -8,16 +8,20 @@ It contains relevant attributes as shown below
 */
 class Process {
  public:
-  int Pid();                               // TODO: See src/process.cpp
-  std::string User();                      // TODO: See src/process.cpp
-  std::string Command();                   // TODO: See src/process.cpp
-  float CpuUtilization();                  // TODO: See src/process.cpp
-  std::string Ram();                       // TODO: See src/process.cpp
-  long int UpTime();                       // TODO: See src/process.cpp
-  bool operator<(Process const& a) const;  // TODO: See src/process.cpp
+  int Pid();
+  std::string User();
+  std::string Command();
+  float CpuUtilization() const;
+  std::string Ram();
+  long int UpTime();
+  bool operator<(Process const& a) const;
 
-  // TODO: Declare any necessary private members
+  Process(int pid);
+
  private:
+  int pid_;
+  std::string user_;
+  std::string command_;
 };
 
 #endif
