c66b5843c14f972ad5122e41e64b1549993a842c
regokan <regokan@gmail.com>
2024-10-07 08:05:54 +0500

    feat implement ElapsedTime helper to format seconds into HH:MM:SS

--- src/format.cpp
+++ src/format.cpp
@@ -4,8 +4,25 @@
 
 using std::string;
 
-// TODO: Complete this helper function
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
-// REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds [[maybe_unused]]) { return string(); }
+string Format::ElapsedTime(long seconds) {
+  long hours = seconds / 3600;
+  long minutes = (seconds % 3600) / 60;
+  long seconds_left = (seconds % 3600) % 60;
+
+  string hours_str = std::to_string(hours);
+  string minutes_str = std::to_string(minutes);
+  string seconds_str = std::to_string(seconds_left);
+
+  if (hours < 10) {
+    hours_str = "0" + hours_str;
+  }
+  if (minutes < 10) {
+    minutes_str = "0" + minutes_str;
+  }
+  if (seconds_left < 10) {
+    seconds_str = "0" + seconds_str;
+  }
+  return hours_str + ":" + minutes_str + ":" + seconds_str;
+}
