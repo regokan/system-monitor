2a5dc9db78f1e00b560fe20f1d55fb0b52baded8
regokan <regokan@gmail.com>
2024-10-07 08:16:53 +0500

    feat calculate aggregate CPU utilization

--- src/processor.cpp
+++ src/processor.cpp
@@ -1,4 +1,16 @@
 #include "processor.h"
+#include "linux_parser.h"
 
-// TODO: Return the aggregate CPU utilization
-float Processor::Utilization() { return 0.0; }
+// Return the aggregate CPU utilization
+float Processor::Utilization() {
+  std::vector<std::string> cpu_utilizations = LinuxParser::CpuUtilization();
+  float total_utilization = 0.0;
+
+  // Sum the individual CPU utilizations
+  for (const std::string& utilization_str : cpu_utilizations) {
+    total_utilization += std::stof(utilization_str);  // Convert string to float
+  }
+
+  // Return the aggregate utilization (average across all CPUs)
+  return total_utilization / cpu_utilizations.size();  // Aggregate utilization
+}
